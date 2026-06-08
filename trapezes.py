eb340f54550a227b47c3381ceb4645228c984e9a
lolo <loic.jacob.1@ens.etsmtl.ca>
2026-06-08 14:06:38 -0400

    création de la fonction tapezes vectorisée en utilisant numpy

--- trapezes.py
+++ trapezes.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def fonction_polynome(x, p1, p2, p3, p4):
     """renvoie la valeur du polynomiale du 3e ordre au point x.
     f(x) = p1 + p2*x + p3*x^2 + p4*x^3 (équation (1) de l'énoncé)"""
@@ -34,3 +36,25 @@ def integration_trapezes_python(a, b, n, p1, p2, p3, p4):
         aire_totale = aire_totale + aire_trapeze
 
     return aire_totale
+
+
+#méthode vectorisée sans for mtn
+
+def integration_trapezes(a, b, n, p1, p2, p3, p4):
+    largeur_segment = (b - a) / n
+    points=nb.linspace (a, b, n+1) #on crée les n+1 points qui vont délimiter les n segments de [a,b]
+    valeurs=fonction_polynome(points, p1, p2, p3, p4) #pour chaque n+1 pointsd on calcule l'image de la fonction
+
+    #par mesure de simplification, on va faire 2 listses qui pour chaque segemnts n, on aura la,valeur de la fonction à gauche et à droite
+
+    f_gauche=valeurs[:-1] #on prend toutes les valeurs sauf le denier
+    f_droite=valeurs[1:] #on prend toutes les valeurs sauf le premier
+
+    #aini pour le premier segment, f_gauche=f(a) et f_droite=f(a+((b-a)/n))
+    aire_trapeze=largeur_segment * (f_gauche + f_droite) / 2
+
+    aire_totale=np.sum(aire_trapeze) #on somme donc l'ensemble des aires de chaque n segments
+
+    return aire_totale
+
+
