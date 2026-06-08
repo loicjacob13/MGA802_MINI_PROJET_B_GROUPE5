3c119b4417319a1ba30c53e9ed87e8fec04d40d5
lolo <loic.jacob.1@ens.etsmtl.ca>
2026-06-08 18:17:06 -0400

    ajout de fonctions pour plot l'erreur selon les méthodes et selon le nombre de segments n et intégration dans le main

--- analyse.py
+++ analyse.py
@@ -20,3 +20,36 @@ def convergence(fonction_integration, a, b, liste_n, p1, p2, p3, p4, valeur_exac
 
     return liste_erreurs
 
+def generer_liste_n(n_min=10,n_max=100000,facteur=2):
+    #génère liste de nombre de segments
+    liste_n = []  # liste des nombres de segments a tester
+    n = n_min  # on commence au minimum
+
+    # Tant qu'on n'a pas dépasse le maximum, on ajoute n et on le multiplie
+    while n <= n_max:
+        liste_n.append(int(n))  # int() au cas ou le facteur est un nombre a virgule
+        n = n * facteur  # on passe a la valeur suivante (plus grande)
+
+    return liste_n
+
+def tracer_convergence(liste_n, dictionnaire_erreurs, nom_fichier="convergence.png"):
+    #on va utiliser le dictionnaire des erreurs : {nom_de_la_methode : liste_des_erreurs}
+    #échelle log-log utilisée
+    #trace graphique de convergence: erreur en fonction du nombre de segments
+    fig, ax = plt.subplots(figsize=(8, 6)) #création figure et des axes
+
+    #pour chaque méthode qui sera dans le dicxtionnaire, on va tracer la courbe
+    for nom_methode in dictionnaire_erreurs:
+        liste_erreurs = dictionnaire_erreurs[nom_methode]
+        ax.loglog(liste_n, liste_erreurs,marker='o', label=nom_methode) #axe log log pour les axes x et y
+
+        #création titre, légende et grille
+        ax.set_xlabel("nombre de segments n")
+        ax.set_ylabel("erreur (en valeur absolue)")
+        ax.set_title("convergence des methodes d'integration")
+        ax.legend()
+        ax.grid(True, which="both")  # which="both" : grille sur grandes (puissances de 10) et petites graduations (valeurs intermédiaires)
+
+        # sauvegarde dans un fichier (l'extension determine le format) et l'affichage
+        fig.savefig(nom_fichier)
+        plt.show()
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from rectangles import rectangles_python, rectangles_numpy
 from trapezes import integration_trapezes, integration_trapezes_numpy
 from simpson import simpson_python, simpson_numpy
 from saisie_utilisateur import demander_bornes, demander_entier_positif, demander_coefficients
-
+from analyse import convergence, generer_liste_n,tracer_convergence
 
 if __name__ == "__main__":
     print("veuillez saisir les paramètres de l'intégration: ")
@@ -17,6 +17,30 @@ if __name__ == "__main__":
 
     #nombre de segments
     n=demander_entier_positif("nombre de segments n: ")
+
+#étude de la convergence de la solution selon le nombre de segments n
+    # on génère automatiquement la liste des n à tester (10, 20, 40, ... )
+    liste_n = generer_liste_n()
+
+    #on va calculer l'erreur pour chaque méthode sur toute la liste de n
+    erreurs_rect_python = convergence(rectangles_python, a, b, liste_n, p1, p2, p3, p4, valeur_exacte)
+    erreurs_rect_numpy = convergence(rectangles_numpy, a, b, liste_n, p1, p2, p3, p4, valeur_exacte)
+    erreurs_trap_python = convergence(integration_trapezes_python, a, b, liste_n, p1, p2, p3, p4, valeur_exacte)
+    erreurs_trap_numpy = convergence(integration_trapezes, a, b, liste_n, p1, p2, p3, p4, valeur_exacte)
+    erreurs_simp_python = convergence(simpson_python, a, b, liste_n, p1, p2, p3, p4, valeur_exacte)
+    erreurs_simp_numpy = convergence(simpson_numpy, a, b, liste_n, p1, p2, p3, p4, valeur_exacte)
+
+#graphique de convergence (erreur vs n), on se sert du dictionnaire explicité dans le module analyse.py
+    # chaque valeur du dictionnaire renvoie une liste, une erreur par valeur de n
+    dictionnaire_erreurs = {
+        "Rectangles": erreurs_rect_python,
+        "Trapèzes": erreurs_trap_python,
+        "Simpson": erreurs_simp_python,
+    }
+    tracer_convergence(liste_n, dictionnaire_erreurs)
+    
+
+
 
     #solution analytique de l'intégrale
     valeur_exacte=solution_analytique(a,b,p1,p2,p3,p4)
@@ -68,4 +92,5 @@ if __name__ == "__main__":
     print("MÉTHODE DE SIMPSON")
     print(f"  Python : aire = {aire_simp_python:.10f}   erreur = {erreur_simp_python:.2e}")
     print(f"  NumPy  : aire = {aire_simp_numpy:.10f}   erreur = {erreur_simp_numpy:.2e}")
+
 
